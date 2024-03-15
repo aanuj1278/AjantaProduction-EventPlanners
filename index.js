5b119d35d0f5690524d4ca698d50f1e2404cf233
Anuj Agarwal <anujagarwal@cyntexa.com>
2024-03-15 11:26:16 +0530

    added index.html file

---
+++ index.js
@@ -0,0 +1,58 @@
+function sendEmail() {
+    console.log('OUTPUT : email called');
+    let fromName = document.getElementById('form4Example1').value;
+    let fromEmail = document.getElementById('form4Example2').value;
+    let subject = document.getElementById('form4Example3').value;
+    let phoneNumber = document.getElementById('form4Example5').value;
+    let fromMessage = document.getElementById('form4Example4').value;
+
+
+    
+    emailjs.init({
+        publicKey: 'WJcVfezltZZ19ymWE',
+        blockHeadless: true,
+        blockList: {
+          list: ['foo@emailjs.com', 'bar@emailjs.com'],
+          watchVariable: 'aanj1278@gmail.com',
+        },
+        limitRate: {
+          id: 'app',
+          throttle: 10000,
+        },
+      });
+
+
+        var templateParams = {
+            from_name: fromName,
+            to_name : 'Ajanta wedding planners',
+            message : fromMessage,
+            from_email : fromEmail,
+            phone_number : phoneNumber
+          };
+          
+          emailjs.send('service_zg45zpn', 'template_vivef54', templateParams).then(
+            (response) => {
+                alert('Your mail is sent!');
+                document.getElementById('form4Example1').value = '';
+                document.getElementById('form4Example2').value = '';
+                document.getElementById('form4Example3').value = '';
+                document.getElementById('form4Example5').value = '';
+                document.getElementById('form4Example4').value = '';
+            },
+            (error) => {
+              console.log('FAILED...', error);
+            },
+          );
+}
+
+window.addEventListener('scroll', function (evt) {
+    if (window.scrollY == 0) {
+        let ele = document.getElementById('header');
+        ele.style.removeProperty("background-color");
+    }
+    if(window.scrollY >= 200){
+        let ele = document.getElementById('header');
+        ele.style.setProperty("background-color", "black");
+    }
+  
+  });
