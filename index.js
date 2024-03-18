1aba25e82e665768aea20603f6878c8e14e2f27c
Anuj Agarwal <anujagarwal@cyntexa.com>
2024-03-18 12:09:02 +0530

    done small changes

--- index.js
+++ index.js
@@ -45,19 +45,19 @@ function sendEmail() {
           );
 }
 
-window.addEventListener('scroll', function (evt) {
-  console.log('OUTPUT : x axis->',screen.width);
-    if (window.scrollY == 0) {
-      console.log('OUTPUT : 51');
-      let ele = document.getElementById('header');
-        if(screen.width > 768){
-          console.log('OUTPUT : 54')
-          ele.style.removeProperty("background-color"); 
-        }
-    }
-    if(window.scrollY >= 200){
-      console.log('OUTPUT : 59')
-        let ele = document.getElementById('header');
-        ele.style.setProperty("background-color", "black");
-    }
-  });
+// window.addEventListener('scroll', function (evt) {
+//   console.log('OUTPUT : x axis->',screen.width);
+//     if (window.scrollY == 0) {
+//       console.log('OUTPUT : 51');
+//       let ele = document.getElementById('header');
+//         if(screen.width > 768){
+//           console.log('OUTPUT : 54')
+//           ele.style.removeProperty("background-color"); 
+//         }
+//     }
+//     if(window.scrollY >= 200){
+//       console.log('OUTPUT : 59')
+//         let ele = document.getElementById('header');
+//         ele.style.setProperty("background-color", "black");
+//     }
+//   });
